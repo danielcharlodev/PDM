d8500b04150e1067dd64e93b8f60cc51ca4a4000
danielcharlodev <danielcharlo.dev@gmail.com>
2026-04-01 16:45:31 -0300

    31-03-2026

---
+++ AULA08/atividade1/lib/main.dart
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Filament\Resources\Roles;
+
+use App\Filament\Resources\Roles\Pages\CreateRole;
+use App\Filament\Resources\Roles\Pages\EditRole;
+use App\Filament\Resources\Roles\Pages\ListRoles;
+use App\Filament\Resources\Roles\Pages\ViewRole;
+use App\Filament\Resources\Roles\Schemas\RoleForm;
+use App\Filament\Resources\Roles\Schemas\RoleInfolist;
+use App\Filament\Resources\Roles\Tables\RolesTable;
+// use App\Models\Role;
+use Spatie\Permission\Models\Role;
+use BackedEnum;
+use Filament\Resources\Resource;
+use Filament\Schemas\Schema;
+use Filament\Support\Icons\Heroicon;
+use Filament\Tables\Table;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Tables\Columns\TextColumn;
+
+class RoleResource extends Resource
+{
+    protected static ?string $model = Role::class;
+
+    public static function canAccess(): bool
+    {
+        return auth()->user()?->hasRole('Admin') ?? false;
+    }
+
+    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedRectangleStack;
+
+    protected static ?string $recordTitleAttribute = 'Cargos e Funções';
+
+    public static function form(Schema $schema): Schema
+    {
+        return RoleForm::configure($schema)
+        ->schema([
+            Select::make('permissions')
+            ->label('Permissões de Acesso')
+            ->multiple()
+            ->relationship('permissions', 'name')
+            ->preload()
+            ->columnSpanFull(),
+
+            TextInput::make('name')
+            ->label('Liberação de Menu')
+            ->required(),
+        ]);
+    }
+
+    public static function infolist(Schema $schema): Schema
+    {
+        return RoleInfolist::configure($schema);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return RolesTable::configure($table)
+        ->columns([
+            TextColumn::make('permissions.name')
+            ->label('Permuissões de Acesso')
+            ->searchable()
+            ->sortable(),
+
+            TextColumn::make('name')
+            ->label('Liberação de Menu')
+            ->searchable()
+            ->sortable(),
+        ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => ListRoles::route('/'),
+            'create' => CreateRole::route('/create'),
+            'view' => ViewRole::route('/{record}'),
+            'edit' => EditRole::route('/{record}/edit'),
+        ];
+    }
+}
